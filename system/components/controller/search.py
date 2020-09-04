e2302f23578d63c5d3b7aedbafda74deb35cc8f1
felipeboffnunes <felipeboffnunes@protonmail.com>
2020-09-04 17:14:40 -0300

    rremove proxies

--- system/components/controller/search.py
+++ system/components/controller/search.py
@@ -3,8 +3,6 @@
 # External Libraries
 from scholarly import scholarly
 # Components
-#   Controller
-from system.components.controller.proxy import make_collector, setup_new_proxies
 #   Model
 from system.components.model.database import populate_author, create_db, get_authors_db
 # Path
@@ -15,24 +13,8 @@ def select_author(name):
 # Get author
 def get_author(name):
     if name != None:
-        print("enter get author")
-        # Setup working proxy
-        def setup_proxy():
-            http_collector = make_collector()
-            print("got htto")
-            while True:
-                http = setup_new_proxies(http_collector)
-                print("got http")
-                print("Trying proxy:", http["http"], http["https"])
-                proxy_works = scholarly.use_proxy(http=http["http"], https=http["https"])
-                if proxy_works:
-                    break
-            print("Working proxy:", http["http"], http["https"])
-            return True
         # Setup tor browser
         def setup_tor():
-            #working_proxy = setup_proxy()
-            #if working_proxy:
             try: 
                 scholarly.launch_tor(tor_path)
             except Exception as e:
