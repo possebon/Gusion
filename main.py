5a7972ded28fe1e6335f8c0adb8f86cc79a95b77
felipeboffnunes <felipeboffnunes@protonmail.com>
2020-11-09 22:04:37 -0300

    procfile

--- main.py
+++ main.py
@@ -10,7 +10,5 @@
 # adicionar grupos para pesquisadores
 from system.app import app
 
-server = app.server
-
 if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ server.py
@@ -0,0 +1,3 @@
+from system.app import app
+
+server = app.server
